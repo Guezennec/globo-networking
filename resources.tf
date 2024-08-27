d72210dc0b722d8d79190ec1446dd8e4e5950f2b
i.guezennec <i.guezennec@gmail.com>
2024-08-27 12:41:52 +0200

    Add third subnet and tags

--- resources.tf
+++ resources.tf
@@ -17,7 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-
+    Environment = var.environement
+    BillingCode = var.billing_code
   }
 }
 
